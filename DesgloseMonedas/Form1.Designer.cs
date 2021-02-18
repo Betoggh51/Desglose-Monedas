fe7bc45aee4cd6702a898d26405e5abc18e17f49
Ivan Gonzalez <ivan@tagshelf.com>
2021-02-18 08:56:46 -0400

    Uploads the project

---
+++ DesgloseMonedas/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace DesgloseMonedas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Monto:";
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(195, 62);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(148, 22);
+            this.txtMonto.TabIndex = 1;
+            this.txtMonto.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(388, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 62);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 142);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(597, 213);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 391);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ DesgloseMonedas/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesgloseMonedas
+{
+    public partial class Form1 : Form
+    {
+        int papeleta2000 = 0, papeleta1000 = 0, papeleta500 = 0, papeleta200 = 0, papeleta100 = 0, papeleta50 = 0, moneda25 = 0, moneda10 =0, moneda1 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtMonto.Text == "")
+            {
+                MessageBox.Show("Por favor, verifique que la casilla de 'monto' esté llena");
+            }
+            else
+            {
+                calcularDesglose();
+                llenarDataGridView();
+            }
+        }
+
+        private void calcularDesglose()
+        {
+            try
+            {
+                int monto = int.Parse(txtMonto.Text);
+
+                papeleta2000 = monto / 2000;
+                monto = monto - (2000 * papeleta2000);
+
+                papeleta1000 = monto / 1000;
+                monto = monto - (1000 * papeleta1000);
+
+                papeleta500 = monto / 500;
+                monto = monto - (500 * papeleta500);
+
+                papeleta200 = monto / 200;
+                monto = monto - (200 * papeleta200);
+
+                papeleta100 = monto / 100;
+                monto = monto - (100 * papeleta100);
+
+                papeleta50 = monto / 50;
+                monto = monto - (50 * papeleta50);
+
+                moneda25 = monto / 25;
+                monto = monto - (25 * moneda25);
+
+                moneda10 = monto / 10;
+                monto = monto - (10 * moneda10);
+
+                moneda1 = monto;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al calcular el desglose de monedas: " + ex.Message);
+                return;
+            }
+            
+        }
+
+        private void llenarDataGridView()
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+
+                dt.Columns.Add("Papeletas 2000");
+                dt.Columns.Add("Papeletas 1000");
+                dt.Columns.Add("Papeletas 500");
+                dt.Columns.Add("Papeletas 200");
+                dt.Columns.Add("Papeletas 100");
+                dt.Columns.Add("Papeletas 50");
+                dt.Columns.Add("Monedas 25");
+                dt.Columns.Add("Monedas 10");
+                dt.Columns.Add("Monedas 1");
+
+                DataRow fila = dt.NewRow();
+
+                fila["Papeletas 2000"] = papeleta2000;
+                fila["Papeletas 1000"] = papeleta1000;
+                fila["Papeletas 500"] = papeleta500;
+                fila["Papeletas 200"] = papeleta200;
+                fila["Papeletas 100"] = papeleta100;
+                fila["Papeletas 50"] = papeleta50;
+                fila["Monedas 25"] = moneda25;
+                fila["Monedas 10"] = moneda10;
+                fila["Monedas 1"] = moneda1;
+
+                dt.Rows.Add(fila);
+                dataGridView1.DataSource = dt;
+                dataGridView1.Refresh();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al llenar el Data Grid View: " + ex.Message);
+                return;
+            }
+        }
+
+    }
+}
